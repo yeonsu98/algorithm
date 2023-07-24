4c208e9c681874cce191d9138037bbd1effacb6e
yeonsu98 <yunsu9888@gmail.com>
2023-07-24 19:17:28 +0900

    Day09

--- Day09/2_Quiz1.py
+++ Day09/2_Quiz1.py
@@ -4,15 +4,17 @@ n = int(input())
 li = list(map(int, input().split()))
 
 li.sort()
+print(li)
 
 result = 0  # 총 그룹 수
 count = 0   # 한 그룹의 인원 수
 
 for i in li :
     count += 1      # 현재 그룹의 인원 수 증가
-
+    print(i, count, result)
     if i <= count:  # 그룹의 인원 수 공포도를 넘으면
         result += 1 # 그룹 하나를 결성
+        print(i, count, result)
         count = 0   # 다음 그룹의 인원을 세기 위해 초기화
 
 print(result)
